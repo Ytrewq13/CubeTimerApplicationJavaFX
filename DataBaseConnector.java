50aa7c357e5ff0504b6fac3fa79eca580aff649f
root <l0022901@ncpontefract.ac.uk>
2017-08-30 03:21:08 +0100

    The DataBaseConnector now connects to the database.

--- DataBaseConnector.java
+++ DataBaseConnector.java
@@ -3,6 +3,9 @@ package timerfx;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicReference;
 import javax.script.ScriptEngine;
@@ -14,14 +17,19 @@ public class DataBaseConnector {
 	// IP addresses.
 	private String lanIP;
 	private String wanIP;
+	private String port;
 
 	// Database details.
+	private String database;
 	private String driver;
 	private String userTable;
 	private String sessionTable;
 	private String timeTable;
 	private String username;
 	private String password;
+	
+	// Database fields.
+	private Connection connection;
 
 	// JavaScript parser for getting JSON data.
 	private ScriptEngineManager factory;
@@ -38,10 +46,56 @@ public class DataBaseConnector {
 		try {
 			this.getDetails(".gitignore/dbinfo.json");
 		} catch (IOException | ScriptException ex) {
-			// DONOTHINGFORNOW.
-			// TODO: SOMETHINGINTHEFUTURE.
 			System.out.println(ex);
 		}
+		try {
+			this.connectDB();
+		} catch (SQLException ex) {
+			System.out.println(ex);
+		}
+	}
+	
+	private void connectDB() throws SQLException {
+		if (TimerFX.debug) {
+			System.out.println("Connecting to database...");
+			
+			System.out.println("Using login username: " + this.username);
+			System.out.println("Using login password: " + this.password);
+		}
+		String ip = this.lanIP;
+		// Connect to the database.
+		this.connection = DriverManager.getConnection(
+				"jdbc:"
+				+ this.driver
+				+ "://"
+				+ ip
+				+ ":"
+				+ this.port
+				+ "/"
+				+ this.database,
+				this.username,
+				this.password);
+		/*
+		* Note to self.
+		*
+		* I had an issue for ages which I couldn't figure out.
+		* The Exception I was getting looked like I wasn't sending
+		* a password to the database.
+		* Eventually I tried just changing "this.password" to "password",
+		* which was the password at the time. This made the error go away
+		* and the system connected to the database successfully.
+		*
+		* It turned out the error was being caused by my not setting the
+		* password field in the "getDetails()" method - I had completely
+		* missed out the line.
+		*
+		* TL;DR.
+		* ALWAYS print your variables when you run into an issue like this.
+		* It will save you A LOT of time messing about with user creation
+		* in an ssh terminal on the mysql server.
+		*/
+		// As long as there was no error, we are now connected to the database.
+		if (TimerFX.debug) System.out.println("Connected to database successfully!");
 	}
 
 	private void getDetails(String JSONurl) throws FileNotFoundException, ScriptException, IOException {
@@ -70,11 +124,16 @@ public class DataBaseConnector {
 		HashMap jsonObject = (HashMap) ret.get();
 		//System.out.println(jsonObject.get("username")); // IT WORKS!!!
 		// Get and set the stuff things.
+		this.database = (String) jsonObject.get("database");
 		this.driver = (String) jsonObject.get("driver");
 		this.username = (String) jsonObject.get("username");
+		this.password = (String) jsonObject.get("password");
 		this.userTable = (String) jsonObject.get("usertable");
 		this.sessionTable = (String) jsonObject.get("sessiontable");
 		this.timeTable = (String) jsonObject.get("timetable");
+		this.lanIP = (String) jsonObject.get("lanip");
+		this.wanIP = (String) jsonObject.get("wanip");
+		this.port = (String) jsonObject.get("port");
 	}
 
 	private void defineJSString() {
